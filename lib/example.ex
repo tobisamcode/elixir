52e473f5e49f48f34c67e63c6c227ba6c8f9d537
tobisamcode <samueloluwatobiloba48@gmail.com>
2024-03-25 20:54:02 +0100

    first day coding elixir

---
+++ lib/example.ex
@@ -0,0 +1,21 @@
+defmodule Example do
+  @moduledoc """
+  Documentation for `Example`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Example.hello()
+      :world
+
+  """
+
+  IO.puts('hello tobi!')
+
+  def hello do
+    :world
+  end
+end
